5fb3a7e082e3ee0df421671251f831b38af214f6
tintin9999 <parthk134@gmail.com>
2021-01-19 03:28:24 +0530

    feat: selection of subreddit is dynamic

--- memes.py
+++ memes.py
@@ -1,27 +1,18 @@
+import os
 import praw 
 import random
-import os
-clientId=os.environ['client_id']
-clientSecret=os.environ['client_secret']
-userAgent=os.environ['user_agent']
-reddit = praw.Reddit(client_id=clientId,client_secret=clientSecret,user_agent=userAgent)
 
-subreddits={
-	"regular":["memes","greentext","Animemes"],
-	"dank":["dankmemes","cursedcomments","4chan"],
-	"relatable":["meirl"],
-	# "facepalm":"facepalm"
+config = {
+	"client_id": os.environ["client_id"],
+	"client_secret": os.environ["client_secret"],
+	"user_agent": os.environ["user_agent"],
 }
 
+reddit = praw.Reddit(**config);
+
 def getMemeUrl(category):
-	categories = list(subreddits.keys())
-	if category not in categories:
-		category="regular"
-	print(category)	
-	subred = reddit.subreddit(random.choice(subreddits[category]))
-	posts = subred.top(limit=10)
-	posts=list(posts)
-	r=random.randint(0,len(posts))
-	post=posts[r]
-	url = post.url
-	return url
+	subred = reddit.subreddits.search_by_name(category)[0]
+	posts = list(subred.top(limit=10))
+
+	post = random.choice(posts)
+	return post.url
